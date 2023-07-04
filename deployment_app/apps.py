e02f690a603e69a49f91584e56a1a0134b017e53
sarika nikam <nikam.sarika01@gmail.com>
2023-07-04 09:44:35 +0530

    project created

---
+++ deployment_app/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class DeploymentAppConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'deployment_app'
---
+++ deployment_app/views.py
@@ -0,0 +1,5 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+def index(r):
+    return HttpResponse('<h2>Sample Deployment project </h2>')
